4b6258bf490b77b92e5b309593d7f7f980e1d230
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2024-01-25 21:03:09 +0100

    Changes

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,7 @@
 function loadContent() {
     var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
         document.getElementById("footer").innerHTML = xhttp.responseText;
     }
 };
@@ -9,7 +9,7 @@ xhttp.open("GET", "globalfooter.html", true);
 xhttp.send();
 var xhttp2 = new XMLHttpRequest();
 xhttp2.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
         document.getElementById("header").innerHTML = xhttp2.responseText;
     }
 };
@@ -18,21 +18,17 @@ xhttp2.send();
 }
 
 function loadPage(element) {
-    var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("right").innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "pages/" + element.textContent.toLowerCase().replace(" ", "_") + ".html", true);
-xhttp.send();
+    loadPageName(element.textContent.toLowerCase().replace(" ", "_"))
 }
 
 function loadPageName(name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             document.getElementById("right").innerHTML = xhttp.responseText;
+            if(document.getElementById("right").getElementsByClassName("initScript").length !== 0) {
+                eval(document.getElementById("right").getElementsByClassName("initScript")[0].innerHTML);
+            }
         }
     };
     xhttp.open("GET", "pages/" + name + ".html", true);
